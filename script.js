63d1ada5c50d8fbe811b6e3e91a6c36dc0f2857d
Soup <izahbel209153@student-schoolsofwestfield.org>
2025-06-13 14:08:03 +0000

    NOPUSHUPSSSS

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function setup(){
-// 7 lines of code, including (not in this order)
-let document.innerHTML=("X")
 
+    let ok = document.createElement("button");
+    ok.id="ok";
+    let buttons=document.getElementById("buttons");
+      ok.innerHTML = "OK";
+     buttons.appendChild(ok);
+
+    let cancel = document.createElement("button");
+    cancel.id="cancel";
+      cancel.innerHTML = "Cancel";
+     buttons.appendChild(cancel);
 // appendChild
 // createElement
 // getElementByID
 // innerHTML
-}
+
